fce6c628ef788288497973a38564f23c0720acae
Aleksandr62aa <145801941+Aleksandr62aa@users.noreply.github.com>
2023-10-29 18:04:19 +0200

    Delete CV/Yolo_kamera/Video_kamera_Yolov8_py.py

--- CV/Yolo_kamera/Video_kamera_Yolov8_py.py
+++
@@ -1,8 +0,0 @@
-# Setup YOLO using Python
-from ultralytics import YOLO
-
-# load model
-model = YOLO("yolov8m.pt")
-
-# predict model, video camera "0"
-results = model.predict(source='0', show=True)
